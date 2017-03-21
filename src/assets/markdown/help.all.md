24dd8c1e06638a1358121085e430e1cf984bd072
Chris Anselmo <christopherwk210@gmail.com>
2017-03-21 15:08:27 -0400

    Removed unused images, refactored help

---
+++ src/assets/markdown/help.all.md
@@ -0,0 +1,20 @@
+Hi, I'm GameMakerBot. Here are my available commands:
+```
+!help      - outputs this message
+
+!role      - toggles a role on and off
+             usage: !role [role]
+             available roles: 'voip'
+
+!resources - outputs a list of trusted resources to assist with your GameMaker Studio journey
+
+!docs      - outputs the URL to the documentation of a GML function
+             usage: !docs [function_name] [optional: version]
+             available versions: 'gms1','gms2'; defaults to 'gms2'
+             example: !docs draw_sprite
+             example: !docs draw_sprite gms2
+             example: !docs draw_sprite gms1
+
+```
+
+I can also assist you with your code formatting and auto generate GMLive snippets for you.
--- src/lib/parse-message.js
+++ src/lib/parse-message.js
@@ -5,11 +5,14 @@ const fs = require("fs");
 const pm = require('./pm');
 const ids = require('../assets/json/ids.json');
 const roleControl = require('./roleControl.js');
-const help = require('./help.js');
+const docs = require('./docs.js');
 
 // Project data
 const welcome = fs.readFileSync('./src/assets/markdown/welcome.md', 'utf8');
 const resources = fs.readFileSync('./src/assets/markdown/resources.md', 'utf8');
+const help = {
+  all: fs.readFileSync('./src/assets/markdown/help.all.md', 'utf8')
+};
 
 const run = function (msg) {
     //Clean punctuation and symbols from messages
@@ -38,69 +41,21 @@ const run = function (msg) {
           msg.author.sendMessage(resources);
           break;
         case "HELP":
-            let command = "all";
+          let command = "all";
 
-            switch (command) {
-                case "all":
-                  let description =
-`
-\`\`\`\n
-!help      - outputs this message \n\n
-!role      - toggles a role on and off \n
-         usage: !role [role] \n
-         available roles: 'voip' \n\n
-!resources - outputs a list of trusted resources to assist with your GameMaker Studio journey\n\n
-!docs      - outputs the URL to the documentation of a GML function \n
-         usage: !docs [function_name] [optional: version] \n
-         available versions: 'gms1','gms2'; defaults to 'gms2' \n
-         example: !docs draw_sprite\n
-         example: !docs draw_sprite gms2\n
-         example: !docs draw_sprite gms1\n\n
-\`\`\`\n\n
-I can also assist you with your code formatting and auto generate GMLive snippets for you.\n\n\n
-`;
-                  msg.author.sendEmbed({
-                    color: 26659,
-                    // title: 'Hi, I\'m GameMakerBot. Here are my available commands:',
-                    // description: description,
-                    image: {url: 'http://i.imgur.com/N95fl5E.png'},
-                    timestamp: new Date(),
-                    footer: {
-                      text: 'This is an automated message'
-                    }
-                  }).catch(err => console.log(err));
-                    // msg.author.sendMessage(
-                    //   "Hi, I'm GameMakerBot. Here are my available commands: \n" +
-                    //   "```" +
-                    //   "!help      - outputs this message \n\n" +
-                    //   "!role      - toggles a role on and off \n" +
-                    //   "             usage: !role [role] \n" +
-                    //   "             available roles: 'voip' \n\n" +
-                    //   "!resources - outputs a list of trusted resources to assist with your GameMaker Studio journey\n\n" +
-                    //   "!docs      - outputs the URL to the documentation of a GML function \n" +
-                    //   "             usage: !docs [function_name] [optional: version] \n" +
-                    //   "             available versions: 'gms1','gms2'; defaults to 'gms2' \n" +
-                    //   "             example: !docs draw_sprite\n" +
-                    //   "             example: !docs draw_sprite gms2\n" +
-                    //   "             example: !docs draw_sprite gms1\n\n" +
-                    //   "```\n\n" +
-                    //   "I can also assist you with your code formatting and auto generate GMLive snippets for you.\n\n\n"
-                    // )
-                    // .then(() => msg.author.sendMessage("**This is how you add code blocks to your messages in Discord:**"), err => console.log(err))
-                    // .then(() => msg.author.sendFile('./gmbot-code-1.png'), err => console.log(err))
-                    // .then(() => msg.author.sendMessage("**To have your code automatically formatted and enable syntax highlighting use the `clean-code` syntax:**"), err => console.log(err))
-                    // .then(() => msg.author.sendFile('./gmbot-code-2.png'), err => console.log(err))
-                    // .then(() => msg.author.sendMessage("**To generate a GMLive snippet in your message use the `gmlive` syntax:**"), err => console.log(err))
-                    // .then(() => msg.author.sendFile('./gmbot-code-3.png'), err => console.log(err));
-                    break;
-            }
-
-            break;
+          switch (command) {
+            case "all":
+              msg.author.sendMessage(help.all)
+              .then(() => msg.author.sendFile('./src/assets/images/help.all.png'), err => console.log(err))
+              .catch(err => console.log(err));
+              break;
+          }
+          break;
         case "ROLE":
           roleControl.control.toggleRole(msg, args.slice(1));
           break;
         case "DOCS":
-          help.control.run(msg, args);
+          docs.control.run(msg, args);
           break;
       }
 
