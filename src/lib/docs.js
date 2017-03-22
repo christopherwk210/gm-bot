13bc08e3a10af00e4d0d380d54d8ec4c05ddf9f2
Kyle Askew <net8floz@gmail.com>
2017-03-22 08:07:33 -0700

    derp?

--- src/lib/docs.js
+++ src/lib/docs.js
@@ -2,74 +2,73 @@ const validate = require('./validate.js');
 const vm = require('vm');
 const http = require('http');
 const concat = require('concat-stream');
+const gms1 = require('./searchdat-gms1');
 
-const control = {
-  run: function(msg, args) {
-    let version = "gms2";
+const control = { 
+    run: function (msg, args) {
+        let version = "gms2";
 
-    if (args.length > 2) {
-      version = args[2];
-    } else if (args.length == 1) {
-      msg.author.sendMessage("You did not include a function name. Type `!help` for help with commands.");
-      return;
+        if (args.length > 2) {
+            version = args[2];
+        } else if (args.length == 1) {
+            msg.author.sendMessage("You did not include a function name. Type `!help` for help with commands.");
+            return;
+        }
+
+        switch (version) {
+            case "gms1":
+                if (validate.gml.gms1(args[1])) {
+                    this.helpUrlGMS1(msg, args[1]);
+                } else {
+                    msg.author.sendMessage("`" + args[1] + "` was not a recognized GMS1 function. Type `!help` for help with commands.");
+                }
+                break;
+            case "gms2":
+                if (validate.gml.gms2(args[1])) {
+                    this.helpUrlGMS2(msg, args[1]);
+                } else {
+                    msg.author.sendMessage("`" + args[1] + "` was not a recognized GMS2 function. Type `!help` for help with commands.");
+                }
+                break;
+            default:
+                msg.author.sendMessage("`" + version + "` was not a valid option. Type `!help` for help with commands.");
+                break;
+        }
+    },
+    helpUrlGMS2: (msg, fn) => {
+        http.get("http://docs2.yoyogames.com/files/searchdat.js", (res) => {
+            res.setEncoding('utf8');
+            res.pipe(concat({encoding: 'string'}, function (remoteSrc) {
+                let found = false;
+                vm.runInThisContext(remoteSrc, 'remote_modules/searchdat.js');
+                for (var i = 0; i < SearchTitles.length; i++) {
+                    if (SearchTitles[i] == fn) {
+                        msg.channel.sendMessage('Here\'s the GMS2 documentation for ' + fn);
+                        msg.channel.sendMessage(encodeURI('http://docs2.yoyogames.com/' + SearchFiles[i]));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    msg.author.sendMessage("`" + fn + "` was not a recognized GMS2 function. Type `!help` for help with commands.");
+                }
+            }));
+        });
+    },
+    helpUrlGMS1: (msg, fn) => {
+        for (var i = 0; i < gms1.titles.length; i++) {
+            if (gms1.titles[i] == fn) {
+                msg.channel.sendMessage('Here\'s the GMS1 documentation for ' + fn);
+                msg.channel.sendMessage(encodeURI('http://docs.yoyogames.com/' + gms1.files[i]));
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            msg.author.sendMessage("`" + fn + "` was not a recognized GMS2 function. Type `!help` for help with commands.");
+        }
     }
-
-    switch (version) {
-      case "gms1":
-        if (validate.gml.gms1(args[1])) {
-          this.helpUrlGMS1(msg, args[1]);
-        } else {
-          msg.author.sendMessage("`" + args[1] + "` was not a recognized GMS1 function. Type `!help` for help with commands.");
-        }
-        break;
-      case "gms2":
-        if (validate.gml.gms2(args[1])) {
-          this.helpUrlGMS2(msg, args[1]);
-        } else {
-          msg.author.sendMessage("`" + args[1] + "` was not a recognized GMS2 function. Type `!help` for help with commands.");
-        }
-        break;
-      default:
-        msg.author.sendMessage("`" + version + "` was not a valid option. Type `!help` for help with commands.");
-        break;
-    }
-  },
-  helpUrlGMS2: (msg, fn) => {
-    http.get("http://docs2.yoyogames.com/files/searchdat.js", (res) => {
-      res.setEncoding('utf8');
-      res.pipe(concat({encoding: 'string'}, function (remoteSrc) {
-        let found = false;
-        vm.runInThisContext(remoteSrc, 'remote_modules/searchdat.js');
-        for (var i = 0; i < SearchTitles.length; i++) {
-          if (SearchTitles[i] == fn) {
-            msg.channel.sendMessage('Here\'s the GMS2 documentation for ' + fn);
-            msg.channel.sendMessage(encodeURI('http://docs2.yoyogames.com/' + SearchFiles[i]));
-            found = true;
-          }
-        }
-
-        if(!found){
-          msg.author.sendMessage("`" + fn + "` was not a recognized GMS2 function. Type `!help` for help with commands.");
-        }
-      }));
-    });
-  },
-  helpUrlGMS1: (msg, fn) => {
-    http.get("http://docs.yoyogames.com/files/searchdat.js", (res) => {
-      res.setEncoding('utf8');
-      res.pipe(concat({encoding: 'string'}, function (remoteSrc) {
-        let found = false;
-        vm.runInThisContext(remoteSrc, 'remote_modules/searchdat.js');
-        for (var i = 0; i < SearchTitles.length; i++) {
-          if (SearchTitles[i] == fn) {
-            msg.channel.sendMessage('Here\'s the GMS1 documentation for ' + fn);
-            msg.channel.sendMessage(encodeURI('http://docs.yoyogames.com/' + SearchFiles[i]));
-            found = true;
-          }
-        }
-      }));
-    });
-  }
 };
 
 module.exports.control = control;
