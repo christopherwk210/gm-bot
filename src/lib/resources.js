81438b7b94bc81c7614de8224a40eb296c0bb8b8
Chris Anselmo <christopherwk210@gmail.com>
2017-03-25 19:26:02 -0400

    Fixed role issue

--- src/lib/resources.js
+++ src/lib/resources.js
@@ -2,14 +2,18 @@ const fs = require("fs");
 const resources = fs.readFileSync('./src/assets/markdown/resources.md', 'utf8');
 
 const run = function(msg, args) {
-  if (msg.member.roles.find('name', 'admin') || msg.member.roles.find('name', 'rubber duckies')) {
-    if (args[1]) {
-      let id = args[1].replace(/([<>@])/g, '');
-      let user = msg.member.guild.members.get(id);
-      if (user) {
-        user.sendMessage(resources);
+  if ((msg.member) && (msg.member.roles)) {
+    if (msg.member.roles.find('name', 'admin') || msg.member.roles.find('name', 'rubber duckies')) {
+      if (args[1]) {
+        let id = args[1].replace(/([<>@])/g, '');
+        let user = msg.member.guild.members.get(id);
+        if (user) {
+          user.sendMessage(resources);
+        } else {
+          msg.author.sendMessage('An error occurred with your request... Did you mention a valid user?');
+        }
       } else {
-        msg.author.sendMessage('An error occurred with your request... Did you mention a valid user?');
+        msg.author.sendMessage(resources);
       }
     } else {
       msg.author.sendMessage(resources);
