e11a4fc664de83a34e3e8f1194aa2e871c78fcf0
Chris Anselmo <christopherwk210@gmail.com>
2017-03-27 16:58:32 -0400

    derp

--- src/express/express.js
+++ src/express/express.js
@@ -4,10 +4,7 @@ var app = express();
 
 const run = function(bot) {
   app.get('/', function (req, res) {
-    let channels = bot.channels.findAll('type','text');
-    for (var i = 0; i < channels.length; i++) {
-      console.log(channels[i].guild);
-    }
+    console.log(bot.guilds.findAll('name','/r/GameMaker'));
     res.send('echo');
   });
 
