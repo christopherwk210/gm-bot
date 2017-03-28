9b113fac92f560eb117a372580bb28a845a5ec32
Chris Anselmo <christopherwk210@gmail.com>
2017-03-27 21:22:55 -0400

    temp comment

--- src/express/express.js
+++ src/express/express.js
@@ -4,17 +4,19 @@ var app = express();
 
 const run = function(bot) {
   app.get('/text_channels', function (req, res) {
-    let channels = bot.guilds.findAll('name','/r/GameMaker').channels.findAll('type', 'text');
-    let sendData = {
-      channels: []
-    };
-    for (let i = 0; i < channels.length; i++) {
-      sendData.channels.push({
-        id: channels[i].id,
-        name: channels[i].name
-      });
-    }
-    res.send(JSON.stringify(sendData));
+    console.log(bot.guilds.findAll('name','/r/GameMaker').channels);
+    // let channels = bot.guilds.findAll('name','/r/GameMaker').channels.findAll('type', 'text');
+    // let sendData = {
+    //   channels: []
+    // };
+    // for (let i = 0; i < channels.length; i++) {
+    //   sendData.channels.push({
+    //     id: channels[i].id,
+    //     name: channels[i].name
+    //   });
+    // }
+    // res.send(JSON.stringify(sendData));
+    res.send('echo');
   });
 
   app.listen(8080, function () {
