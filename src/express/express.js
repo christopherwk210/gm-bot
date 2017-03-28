4dbbe2aa5dd4364ac038cbd3c2b523bc446703d0
Chris Anselmo <christopherwk210@gmail.com>
2017-03-28 05:58:00 -0400

    Added cors module

--- src/express/express.js
+++ src/express/express.js
@@ -1,9 +1,11 @@
 // Express libs
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const run = function(bot) {
   app.get('//text_channels', function (req, res) {
