b1352ed96be69a643a8e139d13bffd2f65cbc876
Chris Anselmo <christopherwk210@gmail.com>
2017-03-27 21:23:55 -0400

    Fix findAll

--- src/express/express.js
+++ src/express/express.js
@@ -4,18 +4,17 @@ var app = express();
 
 const run = function(bot) {
   app.get('/text_channels', function (req, res) {
-    console.log(bot.guilds.findAll('name','/r/GameMaker').channels);
-    // let channels = bot.guilds.findAll('name','/r/GameMaker').channels.findAll('type', 'text');
-    // let sendData = {
-    //   channels: []
-    // };
-    // for (let i = 0; i < channels.length; i++) {
-    //   sendData.channels.push({
-    //     id: channels[i].id,
-    //     name: channels[i].name
-    //   });
-    // }
-    // res.send(JSON.stringify(sendData));
+    let channels = bot.guilds.find('name','/r/GameMaker').channels.findAll('type', 'text');
+    let sendData = {
+      channels: []
+    };
+    for (let i = 0; i < channels.length; i++) {
+      sendData.channels.push({
+        id: channels[i].id,
+        name: channels[i].name
+      });
+    }
+    res.send(JSON.stringify(sendData));
     res.send('echo');
   });
 
