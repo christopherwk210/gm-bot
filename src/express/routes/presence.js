f19f22cb9185cd4c3b223173ed5fc4e38c296f79
Chris Anselmo <christopherwk210@gmail.com>
2017-03-29 09:14:16 -0400

    Added presence route

---
+++ src/express/routes/presence.js
@@ -0,0 +1,21 @@
+module.exports = function(app, bot) {
+  app.post('//presence', function (req, res) {
+    let status, game;
+    try {
+      status = req.body.status;
+      game = req.body.game;
+    } catch(e) {
+      res.status(400).send({
+        error: 'Response error'
+      });
+      return;
+    }
+
+    let user = bot.user;
+    if (game && (game.length !== 0)) {
+      user.setGame(game);
+    }
+    user.setStatus(status);
+    res.send(user.presence);
+  });
+};
--- src/express/routes/simple_stats.js
+++ src/express/routes/simple_stats.js
@@ -1,12 +1,12 @@
 module.exports = function(app, bot) {
   app.get('//simple_stats', function (req, res) {
-    let channel = bot.guilds.find('name','/r/GameMaker');
+    let guild = bot.guilds.find('name','/r/GameMaker');
 
     let data = {
-      id: channel.id,
-      name: channel.name,
-      memberCount: channel.memberCount,
-      iconUrl: channel.iconUrl,
+      id: guild.id,
+      name: guild.name,
+      memberCount: guild.memberCount,
+      iconUrl: guild.iconUrl,
       status: bot.user.presence.status,
       game: bot.user.presence.game
     };
