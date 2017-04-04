b9ae4e4375f34edee2a19bfba9c1d446cb53744d
Chris Anselmo <christopherwk210@gmail.com>
2017-04-04 07:51:31 -0400

    Removed msg deletion promise catching /shrug

--- src/lib/parse-message.js
+++ src/lib/parse-message.js
@@ -40,7 +40,7 @@ const run = function (msg) {
           break;
         case "RESOURCES":
           handleResources.run(msg, args);
-          msg.delete().catch(console.error);;
+          msg.delete();
           break;
         case "HELP":
           let command = "all";
@@ -52,15 +52,15 @@ const run = function (msg) {
               .catch(err => console.log(err));
               break;
           }
-          msg.delete().catch(console.error);;
+          msg.delete();
           break;
         case "ROLE":
           roleControl.control.toggleRole(msg, args.slice(1));
-          msg.delete().catch(console.error);;
+          msg.delete();
           break;
         case "DOCS":
           docs.control.run(msg, args);
-          msg.delete().catch(console.error);;
+          msg.delete();
           break;
       }
     }
