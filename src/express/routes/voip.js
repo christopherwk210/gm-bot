f748920109c11f75b09a077f8aea25c755eabbee
Chris Anselmo <christopherwk210@gmail.com>
2017-04-05 09:17:07 -0400

    Created voip route

---
+++ src/express/routes/voip.js
@@ -0,0 +1,14 @@
+module.exports = function(app, db) {
+  app.get('//voip', function (req, res) {
+    let halfHourAgo = new Date(Date.now() - 30 * 60000);
+    db.voip.find({ 'timestamp': { $gt: halfHourAgo.getTime() } }, function (err, docs) {
+      if (err) {
+        res.status(500).send({
+          error: 'Server Error'
+        });
+      } else {
+        res.send(dosc);
+      }
+    });
+  });
+};
