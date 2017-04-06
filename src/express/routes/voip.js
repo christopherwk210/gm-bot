c5e929a74ecac044cdd5cb41c451c2091cefca20
Chris Anselmo <christopherwk210@gmail.com>
2017-04-06 09:37:21 -0400

    Added voip days log get

--- src/express/routes/voip.js
+++ src/express/routes/voip.js
@@ -11,4 +11,36 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  app.get('//voip/days/:days', function (req, res) {
+    let days;
+    try {
+      days = Number(req.params.days);
+    } catch(e) {
+      res.status(400).send({
+        error: 'Request error'
+      });
+      return;
+    }
+
+    if (isNaN(days)) {
+      res.status(400).send({
+        error: 'Request error'
+      });
+      return;
+    }
+
+
+
+    let history = new Date(Date.now() - (3600000 * (24 * days)));
+    db.voip.find({ 'timestamp': { $gt: history.getTime() } }, function (err, docs) {
+      if (err) {
+        res.status(500).send({
+          error: 'Server Error'
+        });
+      } else {
+        res.send(docs);
+      }
+    });
+  });
 };
