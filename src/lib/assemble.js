6d0c8b2078a15bb242675538385dd9ebe3912d99
Chris Anselmo <christopherwk210@gmail.com>
2017-05-05 11:17:45 -0400

    fix error

--- src/lib/assemble.js
+++ src/lib/assemble.js
@@ -1,15 +1,14 @@
 const assemble = function(msg, args) {
-  console.log(msg);
-  console.log(args);
   if ((msg.member) && (msg.member.roles)) {
     if (msg.member.roles.find('name', 'admin') || msg.member.roles.find('name', 'admins') || msg.member.roles.find('name', 'rubber duckies')) {
       if (args.length === 1) {
         if (msg.guild) {
           let ducks = msg.guild.roles.find('name', 'rubber ducks');
-          ducks.setMentionable(true).then(r => {
-            msg.channel.sendMessage('@rubber_duckies assemble!').catch(console.error);;
-            ducks.setMentionable(false).catch(console.error);
-          }).catch(console.error);
+          console.log(msg.guild.roles);
+          // ducks.setMentionable(true).then(r => {
+          //   msg.channel.sendMessage('@rubber_duckies assemble!').catch(console.error);;
+          //   ducks.setMentionable(false).catch(console.error);
+          // }).catch(console.error);
         }
       }  
     }
