a13f8a05b500b7fa0c1c328f7c2bfc09f08d8c78
Chris Anselmo <christopherwk210@gmail.com>
2017-06-19 12:57:47 -0400

    streamer revoking

--- src/lib/streamer.js
+++ src/lib/streamer.js
@@ -1,17 +1,28 @@
 const run = function(msg, args) {
-
   if ((msg.member) && (msg.member.roles)) {
+
+    let alreadyHas = false;
     let streamerRole;
     msg.member.guild.roles.forEach(role => {
       if (role.name.indexOf('stream') !== -1) {
         streamerRole = role.id;
+        alreadyHas = true;
       }
     });
-    msg.member.addRole( streamerRole ).then(res => {
-      console.log(res);
-    }, err => {
-      console.log(err);
-    });
+
+    if (alreadyHas) {
+      msg.member.removeRole(streamerRole).then(res => {
+        //console.log(res)
+      }, err => {
+        //console.log(err)
+      });
+    } else {
+      msg.member.addRole( streamerRole ).then(res => {
+        //console.log(res);
+      }, err => {
+        //console.log(err);
+      });
+    }
   } else {
     msg.author.sendMessage('This function is not applicable outside of the /r/GameMaker server.');
   }
