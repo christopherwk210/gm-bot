c2e78e43ac76d9e183872952a3b1febb9882df0b
Chris Anselmo <christopherwk210@gmail.com>
2017-09-12 22:25:36 -0700

    Added prettifier test

--- test/lib/modifiers/prettifier.test.js
+++ test/lib/modifiers/prettifier.test.js
@@ -5,6 +5,7 @@ const assert = require('assert');
 const prettifier = require('../../../src/lib/modifiers/prettifier.js');
 
 let sampleMessage = {
+  author: 'test',
   content: '```clean-code if (condition) { return result; } ```',
   delete: () => new Promise(res => res()),
   channel: {
@@ -16,7 +17,12 @@ let sampleMessage = {
 describe('Clean code modifier', function() {
   it('should correctly match message', function() {
     let res = prettifier(sampleMessage);
-    console.log(res);
-    assert.ok(true);
+    assert.ok(res);
+  });
+
+  it('should correctly clean code', function() {
+    let res = prettifier(sampleMessage);
+    assert.ok(sampleMessage.content.indexOf('\n') === -1);
+    assert.ok(res.indexOf('\n') !== -1);
   });
 });
