55a11cf9b82a39422000da77582d6f49c73e1b71
Chris Anselmo <christopherwk210@gmail.com>
2017-09-12 22:43:05 -0700

    Add validate test suite

---
+++ test/lib/docs/validate.test.js
@@ -0,0 +1,18 @@
+// Node libs
+const assert = require('assert');
+
+// Project libs
+const validate = require('../../../src/lib/docs/validate.js');
+
+// Validate test suite
+describe('Validate', function() {
+  it('should correctly identify gms1 functions', function() {
+    assert.ok( validate.gml.gms1('draw_sprite') );
+    assert.ok( validate.gml.gms1('draw_self') );
+  });
+
+  it('should correctly identify gms2 functions', function() {
+    assert.ok( validate.gml.gms2('gpu_set_blendenable') );
+    assert.ok( validate.gml.gms2('draw_self') );
+  });
+});
