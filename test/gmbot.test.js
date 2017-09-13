30cc83889623ad80ae78efad8a7938576aedb528
Chris Anselmo <christopherwk210@gmail.com>
2017-09-13 08:33:00 -0700

    Added gmbot test suite

---
+++ test/gmbot.test.js
@@ -0,0 +1,74 @@
+// Node libs
+const assert = require('assert');
+
+// Project libs
+const gmbot = require('../gmbot.js');
+
+// Sample image options
+let imageOptions = {
+	imageLog: {
+		timers: []
+	},
+	imageCap: 3,		 // 3 images within
+	imageTimer: 5    // 5ms
+};
+
+// Sample message
+let msg = {
+  delete: function() {
+    hasDeleted = true;
+    return new Promise(res => res());
+  },
+  author: {
+    id: 1,
+    send: function() {
+      return new Promise(res => res());
+    }
+  },
+  member: {
+    highestRole: 'voip'
+  },
+  attachments: {
+    array: function() {
+      return [
+        {
+          height: 1
+        }
+      ];
+    }
+  }
+};
+
+let hasDeleted = false;
+
+// GMBot index file test suite
+describe('GMBot', function() {
+  describe('image handling', function() {
+    it('should prevent image spam', function() {
+      gmbot.handleImages(msg, imageOptions);
+      assert.equal(hasDeleted, false);
+      gmbot.handleImages(msg, imageOptions);
+      assert.equal(hasDeleted, false);
+      gmbot.handleImages(msg, imageOptions);
+      assert.equal(hasDeleted, false);
+      gmbot.handleImages(msg, imageOptions);
+      assert.equal(hasDeleted, true);
+    });
+
+    it('should reset after the correct amount of time', function(done) {
+      gmbot.handleImages(msg, imageOptions);
+      gmbot.handleImages(msg, imageOptions);
+      gmbot.handleImages(msg, imageOptions);
+      gmbot.handleImages(msg, imageOptions);
+      
+      setTimeout(function() {
+        assert.notEqual(imageOptions.imageLog[msg.author.id], 0);
+      }, 4);
+
+      setTimeout(function() {
+        assert.equal(imageOptions.imageLog[msg.author.id], 0);
+        done();
+      }, 6);
+    });
+  });
+});
