f47985843be0a1ae601fca22940b1e96514a1f3b
Chris Anselmo <christopherwk210@gmail.com>
2017-09-13 17:28:00 -0700

    Update to GNU GPL

--- gmbot.js
+++ gmbot.js
@@ -1,3 +1,21 @@
+/**
+ * GameMakerBot
+ * Copyright © 2017 Chris Anselmo <christopherwk210@gmail.com> & contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 // Init discord api
 const Discord = require('discord.js');
 const bot = new Discord.Client();
@@ -280,6 +298,10 @@ process.on('uncaughtException', (err) => {
 		user.send('GMBot has encoutered an uncaught exception. Attempting to a log of the error:\n\n' + err);
 	});
 });
+
+// Copyright information
+console.log('GameMakerBot v' + require('./package.json').version);
+console.log('Copyright © 2017 Chris Anselmo & Contributors\nThis program comes with ABSOLUTELY NO WARRANTY.\n\n');
 
 // Login the bot using the auth token from auth.json
 bot.login(auth.token);
