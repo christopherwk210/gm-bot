fdde52af2cd667e1ffd39d599c8509dc56cd9fce
Chris Anselmo <canselmo@hybridhealth.com>
2018-01-06 15:26:30 -0800

    Accept gmlive routes

--- src/express/middlewares/auth.js
+++ src/express/middlewares/auth.js
@@ -2,7 +2,7 @@ const key = process.env.GMBOTAUTH;
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
-  if (req.path == '//gmlive') return next();
+  if (req.path.indexOf('gmlive') !== -1) return next();
   if (req.path == '//login') return next();
 
   jwt.verify(req.headers.auth, key, function(err, decoded) {
