82c3d5b427be42672b93d2844315aac5c3b082d2
Chris Anselmo <canselmo@hybridhealth.com>
2018-02-07 07:59:58 -0800

    Updated assemble command

--- src/lib/commands/assemble.js
+++ src/lib/commands/assemble.js
@@ -4,25 +4,27 @@
  * @param {Array<string>} args Command arguments
  */
 module.exports = function(msg, args) {
+
   // Make sure this is in the server
   if ((msg.member) && (msg.member.roles)) {
+
     // Make sure nothing else was included with the message
     if (args.length === 1) {
+
       // If a guild is present (redundant)
       if (msg.guild) {
-        // Grab the duck and art duck roles
-        let ducks = msg.guild.roles.find('name', 'rubber duckies');
-        let artducks = msg.guild.roles.find('name', 'art duckies');
+
+        // Grab the duck role
+        let ducks = msg.guild.roles.find('id', '345222443012587520');
 
         // Make them mentionable
         ducks.setMentionable(true).then(r => {
-          artducks.setMentionable(true).then(a => {
-            // Mention them!
-            msg.channel.send(`${r} and ${a} assemble!`).then(() => {
-              // Make them unmentionable again
-              r.setMentionable(false).catch(() => {});
-              a.setMentionable(false).catch(() => {});
-            }).catch(() => {});
+
+          // Mention!
+          msg.channel.send(`${r} assemble!`).then(() => {
+
+            // Unmention!
+            r.setMentionable(false).catch(() => {});
           }).catch(() => {});
         }).catch(() => {});
       }
