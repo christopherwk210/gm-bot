f87ae3469cd67467064251f55179d4373cff70e9
Chris Anselmo <canselmo@hybridhealth.com>
2018-04-15 17:58:57 -0700

    Fixed post install data creation

--- src/tools/post-install.js
+++ src/tools/post-install.js
@@ -7,7 +7,7 @@ let banner = fs.readFileSync('./src/assets/text/banner.txt', 'utf8');
 let intro = fs.readFileSync('./src/assets/text/getting-started.txt', 'utf8');
 
 // 'Touch' giveAwaysData.json to prevent missing file errors on first load
-fs.closeSync(fs.openSync(path.join(__dirname, '../data/giveAwaysData.json'), 'a'));
+fs.closeSync(fs.openSync(path.join(__dirname, '../data/giveAwaysData.json'), 'wx'));
 
 // Clear the console
 console.log('\x1Bc');
