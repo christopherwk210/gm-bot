960cd9fb31d552027d98cec04fff78a25cbfac65
Tony Stroemsnaes <tony.stroemsnaes@gmail.com>
2018-04-20 21:27:40 +0000

    Removed quote dependency and fixed title text

--- src/lib/commands/marketplace.js
+++ src/lib/commands/marketplace.js
@@ -13,11 +13,11 @@ const searchURL = 'https://marketplace.yoyogames.com/search/results?utf8=✓&que
  * Formats a query string to be valid for marketplace searches
  */
 function fixQuery(q) {
-  // Convert whitespaces to plus symbols
-  let fixedSpaced = q.replace(/[\s]/g, '+');
+	// Convert whitespaces to plus symbols
+	let fixedSpaced = q.replace(/[\s]/g, '+');
 
-  // Remove invalid characters
-  return fixedSpaced.replace(/([^a-zA-Z+])/g, '');
+	// Remove invalid characters
+	return fixedSpaced.replace(/([^a-zA-Z+])/g, '');
 }
 
 /**
@@ -32,17 +32,17 @@ function fixQuery(q) {
 
 /**
  * Create a Discord rich embed for a given marketplace search result
- * @param {MarketplaceResult} result 
+ * @param {MarketplaceResult} result
  */
 function createResultEmbed(result) {
-  return new Discord.RichEmbed({
-    title: result.title,
-    url: result.url,
-    description: result.type + '\n' + result.price,
-    color: 26659,
-    timestamp: new Date()
-  })
-  .setThumbnail(result.image);
+	return new Discord.RichEmbed({
+		title: result.title,
+		url: result.url,
+		description: result.type.replace(/s$/i, '') + '\n' + result.price,
+		color: 26659,
+		timestamp: new Date()
+	})
+		.setThumbnail(result.image);
 }
 
 /**
@@ -50,86 +50,87 @@ function createResultEmbed(result) {
  * @param {Message} msg Discord message
  * @param {Array<string>} args Command arguments
 */
-module.exports = async function(msg) {
-  // let useEmbed = !!~msg.indexOf('-1');
-  let query = msg.content.match(/("[\s\S]*")/g);
+module.exports = async function(msg, args) {
 
-  // womp womp
-  if (!query || query.length < 1) {
-    msg.author.send('You must provide a search query: `!mp "query goes in here"`');
-    return;
-  }
+	// Shift away the command, then join the rest of the input into one string
+	args.shift();
+	let query = args.reduce((acc, val) => acc + ' ' + val);
+	// Remove "" double quotes surrounding query, if the user is into that kind of stuff
+	if (query.match(/^"/) && query.match(/"$/)) query = query.slice(1, -1);
 
-  // Get start time to determine how long it takes to get results
-  let startTime = new Date();
+	// womp womp
+	if (!query || query.length < 1) {
+		msg.author.send('You must provide a search query: `!mp "query goes in here"` (The double quotes are optional)');
+		return;
+	}
 
-  // Create a loading message
-  let loadingMessage;
-  try {
-    loadingMessage = await msg.channel.send(`Searching the marketplace for ${query}...`);
-  } catch(e) {}
+	// Get start time to determine how long it takes to get results
+	let startTime = new Date();
 
-  // Remove quotation from the query
-  query = query[0].replace(/"/g, '');
+	// Create a loading message
+	let loadingMessage;
+	try {
+		loadingMessage = await msg.channel.send(`Searching the marketplace for ${query}...`);
+	} catch(e) {}
 
-  // Fix search query
-  let fixedQuery = fixQuery(query);
+	// Fix search query
+	let fixedQuery = fixQuery(query);
 
-  // Construct valid search URL
-  let validSearchURL = searchURL + fixedQuery;
+	// Construct valid search URL
+	let validSearchURL = searchURL + fixedQuery;
 
-  // Launch new chrome page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+	// Launch new chrome page
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-  // Search the marketplace
-  await page.goto(validSearchURL);
+	// Search the marketplace
+	await page.goto(validSearchURL);
 
-  /**
+	/**
    * Array of marketplace asset results
    * @type {MarketplaceResult[]}
    */
-  const result = await page.evaluate(() => {
-    let res = [];
-    
-    // Find all results on page
-    let items = document.querySelectorAll('.feature-game');
+	const result = await page.evaluate(() => {
+		let res = [];
 
-    // Get the first three
-    for (let i = 0; i < Math.min(items.length, 3); i++) {
+		// Find all results on page
+		let items = document.querySelectorAll('.feature-game');
 
-      // Save the link and image path
-      res.push({
-        url: items[i].querySelector('a').href,
-        image: items[i].querySelector('.feature-image').src,
-        title: items[i].querySelector('.feature-link').innerHTML,
-        type: items[i].querySelector('.feature-detail a').innerHTML,
-        price: items[i].querySelector('.feature-price small').innerHTML
-      });
-    }
+		// Get the first three
+		for (let i = 0; i < Math.min(items.length, 3); i++) {
 
-    return Promise.resolve(res);
-  }, 7);
+			// Save the link and image path
+			res.push({
+				url: items[i].querySelector('a').href,
+				image: items[i].querySelector('.feature-image').src,
+				title: items[i].querySelector('.feature-link').title,
+				type: items[i].querySelector('.feature-detail a').innerHTML,
+				price: items[i].querySelector('.feature-price small').innerHTML
+			});
+		}
 
-  // Delete the loading message
-  if (loadingMessage) {
-    try {
-      await loadingMessage.delete();
-    } catch(e) {}
-  }
+		return Promise.resolve(res);
+	}, 7);
 
-  // Finish time
-  let endTime = new Date();
+	// Delete the loading message
+	if (loadingMessage) {
+		try {
+			await loadingMessage.delete();
+		} catch(e) {}
+	}
 
-  if (result.length === 0) {
-    msg.channel.send('No results found!\nYour search was: `' + query + '`');
-  } else {
-    let embed = createResultEmbed(result[0]);
-    embed.setFooter(`Results generated in ${endTime - startTime}ms`);
+	// Finish time
+	let endTime = new Date();
 
-    msg.channel.send(embed);
-  }
+	if (result.length === 0) {
+		msg.channel.send('No results found!\nYour search was: `' + query + '`');
+	} else {
+		let embed = createResultEmbed(result[0]);
+		embed.setFooter(`Results generated in ${endTime - startTime}ms`);
 
-  // Close the browser
-  await browser.close();
-}
+		msg.channel.send(embed);
+	}
+
+	// Close the browser
+	await browser.close();
+};
