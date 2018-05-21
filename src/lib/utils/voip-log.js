c43d66227560e5d72e97b0df9ea4884b45002b58
Chris Anselmo <canselmo@hybridhealth.com>
2018-05-21 13:27:03 -0700

    touched up voip log script

--- src/lib/utils/voip-log.js
+++ src/lib/utils/voip-log.js
@@ -16,54 +16,53 @@
  * @param {*} db Database reference
  */
 module.exports = function(om, nm, db) {
-    // console.log('voice event :' + om.selfMute + ":" + nm.selfMute + ':' + om.selfDeaf + ':' + nm.selfDeaf);   // debug: strange om.selfMute / Deaf behaviour
-    if (!(om.selfMute !== nm.selfMute || om.selfDeaf !== nm.selfDeaf) || // exclude selfmute/death - read as 'dont act upon changes'
-         typeof(om.selfMute) === 'undefined' || typeof(om.selfDeaf) === 'undefined' ) {  // see Note '!!!' 
+  // console.log('voice event :' + om.selfMute + ":" + nm.selfMute + ':' + om.selfDeaf + ':' + nm.selfDeaf);   // debug: strange om.selfMute / Deaf behaviour
+  if (!(om.selfMute !== nm.selfMute || om.selfDeaf !== nm.selfDeaf) || // exclude selfmute/death - read as 'dont act upon changes'
+    typeof(om.selfMute) === 'undefined' || typeof(om.selfDeaf) === 'undefined' ) {  // see Note '!!!' 
 
-        // Action and Channel
-        let action = '';
-        let channel = '';
-        let checked = 0;
+    // Action and Channel
+    let action = '';
+    let channel = '';
+    let checked = 0;
 
-        if (om.voiceChannel) {
-            checked += 1; // used for the switch case
-        }
-        if (nm.voiceChannel) {
-            checked += 2;
-        }
-
-        switch(checked) {
-            case 1: action = 'left';    channel = om.voiceChannel.name; break;
-            case 2: action = 'joined';  channel = nm.voiceChannel.name; break;
-            case 3: action = 'swap';    channel = nm.voiceChannel.name; break;
-            default: console.log('unexpected error - neither current not previous channel defined'); break;
-        }
-
-        // attempt to override action: 'swap' if serverMute/serverDeaf event triggered
-        if (om.serverDeaf !== nm.serverDeaf) { 
-            action = nm.serverDeaf ? 'deaf' : 'undeaf';
-        } else if (om.serverMute !== nm.serverMute) {  // mute event
-            action = nm.serverMute ? 'muted' : 'unmuted';
-        }
-
-        let dataBlob = {
-            timestamp: Date.now(),
-            event: 'voice',
-            action: action,
-            channel: channel,
-            user: om.user.username,
-            displayName: om.displayName,
-            mute: nm.serverMute,
-            deaf: nm.serverDeaf
-        }
-        // [push to DB-server here]
-        // console.log(dataBlob);      // debug
-        db.voip.insert(dataBlob, function(err) {
-            if (err) {
-                console.log('Voip log could not save to database.');
-            } else {
-                console.log('Voip stat successfully logged.');
-            }
-        });
+    if (om.voiceChannel) {
+      checked += 1; // used for the switch case
     }
+    if (nm.voiceChannel) {
+      checked += 2;
+    }
+
+    switch(checked) {
+      case 1: action = 'left';    channel = om.voiceChannel.name; break;
+      case 2: action = 'joined';  channel = nm.voiceChannel.name; break;
+      case 3: action = 'swap';    channel = nm.voiceChannel.name; break;
+      default: console.log('unexpected error - neither current not previous channel defined'); break;
+    }
+
+    // attempt to override action: 'swap' if serverMute/serverDeaf event triggered
+    if (om.serverDeaf !== nm.serverDeaf) { 
+      action = nm.serverDeaf ? 'deaf' : 'undeaf';
+    } else if (om.serverMute !== nm.serverMute) {  // mute event
+      action = nm.serverMute ? 'muted' : 'unmuted';
+    }
+
+    let dataBlob = {
+      timestamp: Date.now(),
+      event: 'voice',
+      action: action,
+      channel: channel,
+      user: om.user.username,
+      displayName: om.displayName,
+      mute: nm.serverMute,
+      deaf: nm.serverDeaf
+    }
+
+    db.voip.insert(dataBlob, function(err) {
+      if (err) {
+        console.log('Voip log could not save to database.');
+      } else {
+        console.log('Voip stat successfully logged.');
+      }
+    });
+  }
 };
