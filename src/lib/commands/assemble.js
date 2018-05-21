090020b33406e9f97d15e4868b50c84a3c059717
Chris Anselmo <canselmo@hybridhealth.com>
2018-05-21 13:33:17 -0700

    Update assemble command to use roleService

--- src/lib/commands/assemble.js
+++ src/lib/commands/assemble.js
@@ -1,9 +1,11 @@
+let roleService = require('../services/role.service');
+
 /**
  * Ping the rubber duckies role
  * @param {Message} msg Discord message
  * @param {Array<string>} args Command arguments
  */
-module.exports = function(msg, args) {
+module.exports = async function(msg, args) {
   // Make sure this is in the server
   if ((msg.member) && (msg.member.roles)) {
 
@@ -14,20 +16,23 @@ module.exports = function(msg, args) {
       if (msg.guild) {
 
         // Grab the duck roles
-        let ducks = msg.guild.roles.find('id', '262926334118985728');
-        let audio = msg.guild.roles.find('id', '398875444360904704');
-        let art = msg.guild.roles.find('id', '345222078577901569');
+        let ducks = roleService.getRoleByID('262926334118985728');
+        let audio = roleService.getRoleByID('398875444360904704');
+        let art = roleService.getRoleByID('345222078577901569');
 
-        ducks.setMentionable(true)
-        .then(() => audio.setMentionable(true))
-        .then(() => art.setMentionable(true))
+        try {
+          await ducks.setMentionable(true);
+          await audio.setMentionable(true);
+          await art.setMentionable(true);
 
-        .then(() => msg.channel.send(`${ducks}, ${audio}, ${art}; Assemble!`))
-        
-        .then(() => ducks.setMentionable(false))
-        .then(() => audio.setMentionable(false))
-        .then(() => art.setMentionable(false))
-        .catch(() => {});
+          await msg.channel.send(`${ducks}, ${audio}, ${art}; Assemble!`);
+
+          await ducks.setMentionable(false);
+          await audio.setMentionable(false);
+          await art.setMentionable(false);
+        } catch(e) {
+          console.log(`Error with assemble command: ${'\n'}${e}`);
+        }
       }
     }
   }
