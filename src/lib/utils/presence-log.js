82ff111cb132541a5010e99ad7588144776c832e
Chris Anselmo <canselmo@hybridhealth.com>
2018-05-21 13:27:32 -0700

    touched up presence log

--- src/lib/utils/presence-log.js
+++ src/lib/utils/presence-log.js
@@ -5,32 +5,30 @@
 */
 
 module.exports = function(sc, db) { // server collection - singular!
-    let dataBlob = {
-        timestamp: Date.now(),
-        event: 'profile'
+  let dataBlob = {
+    timestamp: Date.now(),
+    event: 'profile'
+  }
+  
+  sc.members.forEach(function(e) {
+    let status = e.presence.status;
+    let role = e.highestRole.name;
+
+    if (dataBlob[status] === undefined) { // expand dataBlob as required
+      dataBlob[status] = {};
+    }
+    if (dataBlob[status][role] === undefined) {
+      dataBlob[status][role] = 0;
     }
     
-    sc.members.forEach(function(e) {
-        let status = e.presence.status;
-        let role = e.highestRole.name;
- 
-        if (dataBlob[status] === undefined) { // expand dataBlob as required
-            dataBlob[status] = {};
-        }
-        if (dataBlob[status][role] === undefined) {
-            dataBlob[status][role] = 0;
-        }
-        
-        dataBlob[status][role] += 1; // eg. dataBlob.online.admin = 2;
-    });
+    dataBlob[status][role] += 1; // eg. dataBlob.online.admin = 2;
+  });
 
-    // [push to DB-server here]
-    // console.log(dataBlob);      // debug  
-    db.profile.insert(dataBlob, function(err) {
-        if (err) {
-            console.log('Presence could not be saved to database.');
-        } else {
-            console.log('Presence profile successfully logged.');
-        }
-    });
+  db.profile.insert(dataBlob, function(err) {
+    if (err) {
+      console.log('Presence could not be saved to database.');
+    } else {
+      console.log('Presence profile successfully logged.');
+    }
+  });
 };
