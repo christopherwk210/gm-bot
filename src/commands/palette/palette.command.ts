3fdb7108a6b1601ad7a66f8e07e709e08d69bd68
Chris Anselmo <canselmo@hybridhealth.com>
2018-05-30 09:08:52 -0700

    Implemented alias into palette command

--- src/commands/palette/palette.command.ts
+++ src/commands/palette/palette.command.ts
@@ -12,16 +12,49 @@ export class PaletteCommand implements CommandClass {
   /** Lospec palette page */
   url = 'https://lospec.com/palette-list/';
 
+  /** Contains common palette alias */
+  paletteNameAlias: Alias[] = [
+    {
+      alias: ['edg'],
+      for: 'endesga'
+    },
+    {
+      alias: ['nes'],
+      for: 'Nintendo-Entertainment-System'
+    },
+    {
+      alias: ['jmp'],
+      for: 'JMP-Japanese-Machine-Palette'
+    },
+    {
+      alias: ['dbs8', 'dbs-8', 'dbs 8'],
+      for: 'DawnBringers-8-color'
+    },
+    {
+      alias: ['dbs', 'aseprite-default'],
+      for: 'DawnBringers'
+    }
+  ];
+
   async action(msg: Message, args: string[]) {
     if (args.length === 1) return msg.channel.send('Invalid command usage$! Proper usage: `!palette [palette_name]`');
 
     // Get the palette name
     let paletteName = args.slice(1).join('-').toLowerCase();
+    paletteName = this.handleAlias(paletteName);
 
     // Get the palette page
     let palettePageHTML = await this.getPalettePage(paletteName);
     if (!palettePageHTML || !!~(<string>palettePageHTML).indexOf('The palette you requested could not be found.')) {
-      return msg.channel.send('Palette not found!');
+      
+      // Attempt to get the page with a separated number before we give up
+      paletteName = paletteName.replace(/\d+$/g, '-$&');
+      palettePageHTML = await this.getPalettePage(paletteName);
+
+      // Okay I give up now
+      if (!palettePageHTML || !!~(<string>palettePageHTML).indexOf('The palette you requested could not be found.')) {
+        return msg.channel.send(`${paletteName} palette not found!`);
+      }
     }
 
     // Get page information
@@ -43,6 +76,18 @@ export class PaletteCommand implements CommandClass {
   }
 
   /**
+   * Handles common palette name alias by making replacements to the given name
+   * @param paletteName 
+   */
+  handleAlias(paletteName: string) {
+    this.paletteNameAlias.forEach(aliasMap => {
+      aliasMap.alias.forEach(alias => paletteName = paletteName.replace(alias, aliasMap.for));
+    });
+
+    return paletteName;
+  }
+
+  /**
    * Returns a lospec palette page as HTML
    * @param palette Palette name
    */
@@ -57,4 +102,12 @@ export class PaletteCommand implements CommandClass {
       }).end();
     });
   }
+}
+
+interface Alias {
+  /** All alias to match */
+  alias: string[];
+
+  /** What the alias reference to */
+  for: string;
 }
