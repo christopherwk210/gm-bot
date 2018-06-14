176d31aba7e463c041d4bb38a1b2580a8e2e16b3
Chris Anselmo <canselmo@hybridhealth.com>
2018-06-14 09:38:18 -0700

    Added +1 alias to upvotes, simplified upvote command

--- src/commands/upvote/upvote.command.ts
+++ src/commands/upvote/upvote.command.ts
@@ -1,33 +1,37 @@
-import { Message } from 'discord.js';
+import { Message, Collection } from 'discord.js';
 import { prefixedCommandRuleTemplate, defaultEmbedColor } from '../../config';
 import { Command, CommandClass, detectStaff } from '../../shared';
 
 @Command({
-  matches: ['^'],
+  matches: ['^', '+1'],
   prefix: '',
   position: 0,
   exact: true,
-  delete: false
+  delete: false,
+  wholeMessage: true
 })
 export class UpvoteCommand implements CommandClass {
-    /**
-     * Upvotes message above when user types "^"
-     * @param msg
-     * @param args
-     */
-    action(msg: Message, args: string[]) {
-        if (args.length > 1 || args[0].length > 1) return;
-        msg.channel.fetchMessages({ limit: 6 }).then(fetchedMessages => {
-            let messages = fetchedMessages.array();
-            for (let i = 0; i < messages.length - 1; i++) {
-                if (messages[i].id === msg.id) {
-                    messages[i + 1].react('⬆');
-                    msg.delete();
-                    return;
-                }
-            }
-            messages[0].react('⬆');
-            msg.delete();
-        }).catch(err => console.log(err));
+  /**
+   * Upvotes message above when user types "^"
+   * @param msg
+   * @param args
+   */
+  async action(msg: Message, args: string[]) {
+    let fetchedMessages: Collection<string, Message>;
+
+    try {
+      fetchedMessages = await msg.channel.fetchMessages({ limit: 6 });
+    } catch (e) {
+      return console.log(e);
     }
+
+    const messages = fetchedMessages.array();
+    const found = messages.findIndex(message => message.id === msg.id);
+
+    try {
+      await (!!~found ? messages[found + 1] : messages[0]).react('⬆');
+    } catch (e) {}
+
+    msg.delete();
+  }
 }
