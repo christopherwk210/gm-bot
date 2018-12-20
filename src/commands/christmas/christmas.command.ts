1b0060ddfad491460e7c37037eea0cf9a594660c
Chris Anselmo <canselmo@hybridhealth.com>
2018-12-20 08:54:45 -0800

    made christmas role color assignments persistent

--- src/commands/christmas/christmas.command.ts
+++ src/commands/christmas/christmas.command.ts
@@ -2,34 +2,34 @@ import { Message } from 'discord.js';
 import { prefixedCommandRuleTemplate } from '../../config';
 import { Command, CommandClass, detectStaff, roleService } from '../../shared';
 
+/** Colors to cycle through */
+const colors = {
+  RED: '#ff0000',
+  GREEN: '#009600',
+  WHITE: '#fefefe'
+};
+
+/** Stores the cyclable roles and their current colors */
+let roles = {
+  A: {
+    role: roleService.getRoleByName('A'),
+    color: this.colors.RED
+  },
+  B: {
+    role: roleService.getRoleByName('B'),
+    color: this.colors.GREEN
+  },
+  C: {
+    role: roleService.getRoleByName('C'),
+    color: this.colors.WHITE
+  }
+};
+
 @Command({
   matches: ['christmascycle', 'christmasautocycle', 'christmasstop'],
   ...prefixedCommandRuleTemplate
 })
 export class ChristmasCommand implements CommandClass {
-  /** Colors to cycle through */
-  colors = {
-    RED: '#ff0000',
-    GREEN: '#009600',
-    WHITE: '#fefefe'
-  };
-
-  /** Stores the cyclable roles and their current colors */
-  roles = {
-    A: {
-      role: roleService.getRoleByName('A'),
-      color: this.colors.RED
-    },
-    B: {
-      role: roleService.getRoleByName('B'),
-      color: this.colors.GREEN
-    },
-    C: {
-      role: roleService.getRoleByName('C'),
-      color: this.colors.WHITE
-    }
-  };
-
   /** Interval reference to auto-cycling */
   cycleInterval;
 
@@ -63,12 +63,12 @@ export class ChristmasCommand implements CommandClass {
    */
   cycle() {
     // Turn all available colors into an array
-    let colorArray = Object.values(this.colors);
+    let colorArray = Object.values(colors);
 
     // Iterate over each role
-    Object.keys(this.roles).forEach(key => {
+    Object.keys(roles).forEach(key => {
       // Get the current color of this role
-      let currentColor = this.roles[key].color;
+      let currentColor = roles[key].color;
 
       // Find it's index in the color array
       let currentIndex = colorArray.indexOf(currentColor);
@@ -77,10 +77,10 @@ export class ChristmasCommand implements CommandClass {
       let newIndex = currentIndex === colorArray.length - 1 ? 0 : currentIndex + 1;
 
       // Assign the color with the new index
-      this.roles[key].color = colorArray[newIndex];
+      roles[key].color = colorArray[newIndex];
 
       // Set the color of the role
-      this.roles[key].role.setColor(this.roles[key].color);
+      roles[key].role.setColor(roles[key].color);
     });
   }
 
