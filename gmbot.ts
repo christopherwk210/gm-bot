7fea21e4f8894d164b26f38efdf543b167582b05
Chris Anselmo <canselmo@hybridhealth.com>
2019-09-09 09:36:15 -0700

    Catch raw discord packets to handle reactions on uncached messages

--- gmbot.ts
+++ gmbot.ts
@@ -12,7 +12,7 @@ const bot = new Client();
 import { serverIDs, shouldDieOnException, shouldPrintStackTrace } from './src/config';
 
 // Utils
-import { parseCommandList, parseModifierList, Rule, Type , detectSpamMessage, detectOutsideStaff } from './src/shared';
+import { parseCommandList, parseModifierList, Rule, Type , detectSpamMessage, detectOutsideStaff, detectStaff } from './src/shared';
 import './src/shared/utils/choose';
 
 // Express
@@ -152,6 +152,38 @@ function onBotReactionAdd(reaction: MessageReaction, user: User) {
 function onBotError(error: Error) {
   console.log('\nThe bot client encountered an error:\n', error.message);
 }
+
+// This is to handle emoji reactions on uncached messages
+bot.on('raw', packet => {
+  // We don't want this to run on unrelated packets
+  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+
+  // Grab the channel to check the message from
+  const channel = bot.channels.get(packet.d.channel_id);
+
+  // There's no need to emit if the message is cached, because the event will fire anyway for that
+  if ((channel as any).messages.has(packet.d.message_id)) return;
+
+  // Since we have confirmed the message is not cached, let's fetch it
+  (channel as any).fetchMessage(packet.d.message_id).then(message => {
+      // Emojis can have identifiers of name:id format, so we have to account for that case as well
+      const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+
+      // This gives us the reaction we need to emit the event properly, in top of the message object
+      const reaction = message.reactions.get(emoji);
+
+      // Adds the currently reacting user to the reaction's users collection.
+      if (reaction) reaction.users.set(packet.d.user_id, bot.users.get(packet.d.user_id));
+
+      // Check which type of event it is before emitting
+      if (packet.t === 'MESSAGE_REACTION_ADD') {
+          bot.emit('messageReactionAdd', reaction, bot.users.get(packet.d.user_id));
+      }
+      if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+          bot.emit('messageReactionRemove', reaction, bot.users.get(packet.d.user_id));
+      }
+  });
+});
 
 // Handle process-wide promise rejection
 process.on('unhandledRejection', reason => {
