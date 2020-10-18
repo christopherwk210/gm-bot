07e58a79cb0917c64e8c35df9d1fa9fe8dbc55f8
Jonathan Spira <jjspira@gmail.com>
2020-10-17 22:34:31 -0700

    add prettier and make my life easier

--- src/commands/docs/docs.command.ts
+++ src/commands/docs/docs.command.ts
@@ -25,13 +25,7 @@ declare let SearchFiles;
   matches: ['docs', 'doc'],
   ...prefixedCommandRuleTemplate
 })
-export class DocsCommand implements CommandClass {
-  gms1DocumentationUrls: any;
-
-  constructor() {
-    this.gms1DocumentationUrls = jsonService.files['gms1-docs-urls'];
-  }
-
+addexport class DocsCommand implements CommandClass {
   /**
    * Commence documentation fetching!
    * @param msg
@@ -45,18 +39,22 @@ export class DocsCommand implements CommandClass {
 
     if (args.length === 1) {
       // Throw on unsupplied function
-      msg.author.send('You did not include a function name. Type `!help` for help with commands.');
+      msg.author.send(
+        'You did not include something to find. Type `!help` for help with commands.'
+      );
       return;
     } else if (args.length > 2) {
-      version = args.includes('gms1') || args.includes('GMS1') ? 'GMS1' : 'GMS2';
       if (args.indexOf('-i') !== -1) image = true;
 
       // find a mention tag
-      if (msg.mentions.users.first() !== undefined  && detectStaff(msg.member)) {
+      if (msg.mentions.users.first() !== undefined && detectStaff(msg.member)) {
         whoTag = msg.mentions.users.first();
 
         // check if tagged user is a member of of the server
-        if (msg.mentions.members.first() === undefined || msg.mentions.users.first().id !== msg.mentions.members.first().id) {
+        if (
+          msg.mentions.members.first() === undefined ||
+          msg.mentions.users.first().id !== msg.mentions.members.first().id
+        ) {
           msg.author.send(`<@${whoTag.id}> was not a recognized user.`);
           return;
         }
@@ -68,191 +66,25 @@ export class DocsCommand implements CommandClass {
       whoTag = msg.author;
     }
 
-    // Switch on version
-    switch (version) {
-      case 'GMS1':
-        // Determine if the provided function is a valid GMS1 function
-        if (validateGMS1(args[1])) {
-          // If so, provide the helps
-          this.helpUrlGMS1(msg, args[1], image, whoTag.id);
-        } else {
-          // Otherwise, provide the nopes
-          msg.author.send(`\`${args[1]}\` was not a recognized GMS1 function. Type \`!help\` for help with commands.`);
-        }
-        break;
-      case 'GMS2':
-        // Attempt our new Docs method
-        const possibleFancyEmbed = this.attemptNewDocs(args[1], whoTag);
-        if (possibleFancyEmbed) {
-          msg.channel.send(possibleFancyEmbed);
-          return;
-        }
-
-        // Determine if the provided function is a valid GMS2 function
-        if (validateGMS2(args[1])) {
-          // If so, give 'em the goods
-          this.helpUrlGMS2(msg, args[1], image, whoTag.id);
-        } else {
-          // Otherwise, kick 'em to the curb
-          msg.author.send(`\`${args[1]}\` was not a recognized GMS2 function. Type \`!help\` for help with commands.`);
-        }
-        break;
-      default:
-        // Invalid GMS version (this is actually impossible to reach, here just in case functionality changes)
-        msg.author.send(`\`${version}\` was not a valid option. Type \`!help\` for help with commands.`);
-        break;
+    if (args.some(v => v.toLowerCase().includes('gms1'))) {
+      msg.author.send(
+        'We no longer support Gms1 Documentation, as Gms1.4999 was sunset on June 8 2017'
+      );
     }
 
-  }
-
-  /**
-   * Provide GMS1 doc URL
-   * @param msg The Discord message asking for help
-   * @param fn Function name to lookup
-   * @param image whether to include a screenshot
-   * @param who who to tag about the message
-   */
-  helpUrlGMS1(msg: Message, fn: string, image, who) {
-    let found = false;
-
-    // Loop through valid titles
-    for (let i = 0; i < this.gms1DocumentationUrls.titles.length; i++) {
-      // If we match up with a function
-      if (this.gms1DocumentationUrls.titles[i] === fn) {
-        // Send a screenshot if requested
-        if (image) {
-          this.sendScreenshot(
-            `Here's the GMS1 documentation for \`${fn}\``,
-            `http://docs.yoyogames.com/${this.gms1DocumentationUrls.files[i]}`,
-            msg
-          );
-          return;
-        }
-
-        // Put together a URL and serve it on a silver platter
-        msg.channel.send(`Here's the GMS1 documentation for \`${fn}\`, <@${who}>`);
-        msg.channel.send(encodeURI(`http://docs.yoyogames.com/${this.gms1DocumentationUrls.files[i]}`));
-
-        // We struck gold, ma!
-        found = true;
-        break;
-      }
+    // Attempt our new Docs method
+    const possibleFancyEmbed = this.attemptNewDocs(args[1], whoTag);
+    if (possibleFancyEmbed) {
+      msg.channel.send(possibleFancyEmbed);
+      return;
     }
 
-    // No gold to be found
-    if (!found) {
-      // Tough luck
-      msg.author.send(`\`${fn}\` was not a recognized GMS2 function. Type \`!help\` for help with commands.`);
-    }
-  }
-
-  /**
-   * Provide GMS2 doc URL
-   * @param msg The Discord message asking for help
-   * @param fn Function name to lookup
-   * @param image whether to include a screenshot
-   * @param who who to tag about the message
-   */
-  helpUrlGMS2(msg: Message, fn: string, image, who) {
-    // Download super saucy secret file from YYG server
-    http.get('http://docs2.yoyogames.com/files/searchdat.js', res => {
-      // Read like a normal bot
-      res.setEncoding('utf8');
-
-      // Let's check the goods
-      res.pipe(concat({ encoding: 'string' }, (remoteSrc: any) => {
-        let found = false;
-
-        if (remoteSrc.indexOf('<') === 0) {
-          msg.author.send('GMS2 documentation is currently unavailable. Falling back to GMS1 documentation...');
-          this.action(msg, ['!docs', fn, 'gms1']);
-          return;
-        }
-
-        // Execute in context to access the inner JS
-        vm.runInThisContext(remoteSrc, 'remote_modules/searchdat.js');
-
-        // Loop through newly available SearchTitles (from searchdat.js)
-        for (let i = 0; i < SearchTitles.length; i++) {
-          // If we find the function we're looking for
-          if (SearchTitles[i] === fn) {
-            // Send a screenshot if requested
-            if (image) {
-              this.sendScreenshot(
-                `Here's the GMS2 documentation for \`${fn}\``,
-                `http://docs2.yoyogames.com/${SearchFiles[i]}`,
-                msg
-              );
-              return;
-            }
-
-            // Provide it
-            msg.channel.send(`Here's the GMS2 documentation for \`${fn}\`, <@${who}>`);
-            msg.channel.send(encodeURI(`http://docs2.yoyogames.com/${SearchFiles[i]}`));
-
-            // Indiciate we found it
-            found = true;
-            break;
-          }
-        }
-
-        // If we haven't found jack...
-        if (!found) {
-          // Sorry pal
-          msg.author.send(`\`${fn}\` was not a recognized GMS2 function. Type \`!help\` for help with commands.`);
-        }
-      }));
-    });
-  }
-
-  /**
-   * Takes a screenshot of a GMS docs page and sends it to the discord chat
-   * @param messageText Message to send with screenshot
-   * @param URL Website to take a screenshot of
-   * @param msg
-   */
-  sendScreenshot(messageText: string, URL: string, msg: Message) {
-    msg.channel.send('Loading documentation...').then(async (message: Message) => {
-      // Launch chrome
-      let browser = await puppeteer.launch();
-
-      // Create a new page
-      const page = await browser.newPage();
-
-      // Navigate to the URL
-      await page.goto(URL);
-
-      // Remove the top useless elements of the docs page
-      await page.evaluate(() => new Promise(res => {
-        document.querySelector('table').remove();
-        document.querySelector('br').remove();
-        res();
-      }));
-
-      // Set our viewport to be 1280 wide
-      await page.setViewport({
-        width: 1280,
-        height: 1
-      });
-
-      // Take a screenshot of the full page
-      let image = await page.screenshot({
-        fullPage: true
-      });
-
-      // Close the browser
-      await browser.close();
-
-      // Send the message
-      msg.channel.send(messageText, <any>{
-        file: image,
-        name: 'capture.png'
-      }).then(() => message.delete());
-    });
+    msg.author.send(
+      `\`${args[1]}\` was not a recognized GMS2 function. Type \`!help\` for help with commands.`
+    );
   }
 
   attemptNewDocs(docWord: string, user: User): undefined | RichEmbed {
-
     // New Docs style ability -- query the shared service
     const docInfo = docService.docsFindEntry(docWord);
     if (!docInfo) {
@@ -285,7 +117,10 @@ export class DocsCommand implements CommandClass {
       const funcColor = 3447003;
 
       // Limit our Description to just the first sentence
-      const funcDesc = docInfo.entry.documentation.slice(0, docInfo.entry.documentation.indexOf('.') + 1);
+      const funcDesc = docInfo.entry.documentation.slice(
+        0,
+        docInfo.entry.documentation.indexOf('.') + 1
+      );
 
       // Create our Arguments and sort the strong from the, uh, optional arguments
       const ourArgs: string[] = [];
@@ -312,13 +147,16 @@ export class DocsCommand implements CommandClass {
         title: docInfo.entry.signature,
         url: docInfo.entry.link,
         description: funcDesc,
-        fields: ourArgs.length === 0 ? undefined : [
-          {
-            name: 'Arguments',
-            value: ourArgs.join('\n'),
-            inline: true
-          }
-        ],
+        fields:
+          ourArgs.length === 0
+            ? undefined
+            : [
+                {
+                  name: 'Arguments',
+                  value: ourArgs.join('\n'),
+                  inline: true
+                }
+              ],
         timestamp: new Date(),
         footer: {
           text: `This message was called for ${user.username}`
@@ -334,10 +172,14 @@ export class DocsCommand implements CommandClass {
       const varColor = 1572715;
 
       // Create a nice title with type:
-      const varTitle = docInfo.entry.name + ': *' + docInfo.entry.type.toLowerCase() + '*';
+      const varTitle =
+        docInfo.entry.name + ': *' + docInfo.entry.type.toLowerCase() + '*';
 
       // Limit our Description to just the first sentence
-      const varDesc = docInfo.entry.documentation.slice(0, docInfo.entry.documentation.indexOf('.') + 1);
+      const varDesc = docInfo.entry.documentation.slice(
+        0,
+        docInfo.entry.documentation.indexOf('.') + 1
+      );
 
       const ourEmbed = new RichEmbed({
         color: varColor,
