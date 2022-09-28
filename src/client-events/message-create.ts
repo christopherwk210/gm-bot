faa47412e096d64e4826def7ba4c592d529a271f
Chris <canselmo@hybridhealth.com>
2022-09-27 18:55:56 -0700

    Added wrong code detection

--- src/client-events/message-create.ts
+++ src/client-events/message-create.ts
@@ -1,10 +1,12 @@
 import { Message } from 'discord.js';
 import { handleExplodeCommandMessages } from '../message-handlers/explode-handler.js';
 import { handleHasteCodeBlockMessages } from '../message-handlers/haste-code-block.js';
+import { handleWrongCodeBlockMessages } from '../message-handlers/wrong-code-block.js';
 
 export async function onMessageCreate(message: Message<boolean>) {
   if (message.author.bot) return;
   
   if (await handleExplodeCommandMessages(message)) return;
   await handleHasteCodeBlockMessages(message);
+  await handleWrongCodeBlockMessages(message);
 }
---
+++ src/message-handlers/wrong-code-block.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js';
+
+const match = `(?<!\\S)('''|´´´)([^'´]|[^'´][\\s\\S]*?[^'´])\\1(?!\\S)`;
+export async function handleWrongCodeBlockMessages(message: Message<boolean>) {
+  if (!message.content.match(new RegExp(match, 'g'))) return;
+  let formatted = message.content.replace(new RegExp(match, 'g'), '```js\n$2```');
+
+  // prevent pings
+  formatted = formatted.replace(/(@everyone)/g, '@ everyone');
+  formatted = formatted.replace(/(@here)/g, '@ here');
+
+  await message.reply({
+    content: `Hey! You tried formatting your code with \`\`'''\`\` or \`\`´´´\`\`, ` +
+    `however the correct symbol is the backtick: \`\` \`\`\` \`\`. Here is your message formatted properly:\n\n${formatted}`
+  });
+}
