f955fac251ed0895a5eac36f143da41127cba6bc
Chris <canselmo@hybridhealth.com>
2022-10-20 08:53:26 -0700

    Remove channel patching test

--- src/client-events/message-create.ts
+++ src/client-events/message-create.ts
@@ -1,14 +1,9 @@
-import { Message, Routes } from 'discord.js';
+import { Message } from 'discord.js';
 import { handleExplodeCommandMessages } from '@/message-handlers/explode-handler.js';
 import { handleHasteCodeBlockMessages } from '@/message-handlers/haste-code-block.js';
 import { reactToMessage } from '@/message-handlers/reaction-handler.js';
 import { handleWrongCodeBlockMessages } from '@/message-handlers/wrong-code-block.js';
 import { handleMisc } from '@/message-handlers/misc-handlers.js';
-
-import { REST } from '@discordjs/rest';
-import { token } from '@/data/environment.js';
-const restClient = () => new REST({ version: '7' }).setToken(token);
-const route = Routes.channel('417796218324910094');
 
 export async function onMessageCreate(message: Message<boolean>) {
   if (message.author.bot) return;
@@ -18,14 +13,4 @@ export async function onMessageCreate(message: Message<boolean>) {
   await handleWrongCodeBlockMessages(message);
   await reactToMessage(message);
   await handleMisc(message);
-
-  if (message.content === '__!!~') {
-    const rest = restClient();
-    const result = await rest.patch(route, {
-      body: {
-        name: 'bot_testing_'+Math.floor(Math.random()*100)
-      }
-    });
-    console.log(result);
-  }
 }
