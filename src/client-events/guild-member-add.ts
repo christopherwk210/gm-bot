553ef338f8e759fb15ee4511c3e5af04066b60f6
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2023-09-17 14:10:13 +1000

    Changed join message to mention the starter-pack command

--- src/client-events/guild-member-add.ts
+++ src/client-events/guild-member-add.ts
@@ -9,7 +9,7 @@ Thanks for joining! **Please read the #rules channel thoroughly for our code of 
 
 Swing by **#lounge** to hang out and get to know other members! If you'd like help using GameMaker, please use one of the **#help** channels. If someone is currently being helped, please don't interrupt!
 
-If you are new to GameMaker, and are unsure where to start, you can type \`!starter\` to recieve a list of resources to help you learn!`;
+If you are new to GameMaker, and are unsure where to start, you can run the </starter-pack:1024100590118260826> command to recieve a list of resources to help you learn!`;
 
 export async function onGuildMemberAdd(member: GuildMember) {
   if (member.guild.id !== config.discordIds.guildId) return;
