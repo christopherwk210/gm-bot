1d62e273c2d941ed8c02106a856b7bbf5e1afcff
Chris Anselmo <chris@chrisanselmo.com>
2024-02-23 14:36:45 +0000

    send idle message on help channel timeout

--- src/misc/help-channel-ticker.ts
+++ src/misc/help-channel-ticker.ts
@@ -1,6 +1,6 @@
 import { config } from '@/data/config.js';
 import { db } from '@/database/db.js';
-import { Client, TextChannel } from 'discord.js';
+import { Client, EmbedBuilder, TextChannel } from 'discord.js';
 
 export function setupHelpChannelTicker(client: Client<boolean>) {
   setInterval(() => handleHelpChannelOps(client), 60_000);
@@ -18,6 +18,12 @@ async function handleHelpChannelOps(client: Client<boolean>) {
         channel.parent &&
         channel.parentId === config.discordIds.channelCategories.helpBusy
       ) {
+        const embed = new EmbedBuilder()
+        .setColor(config.defaultEmbedColor)
+        .setDescription(`This channel has been made available again due to inactivity.`)
+        .setTimestamp(new Date());
+
+        channel.send({ embeds: [embed] });
         moveChannelToUnbusy(channel);
       }
     }
