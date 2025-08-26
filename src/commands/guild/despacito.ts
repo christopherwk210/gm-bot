e9fc1f714c4f8416ab7b7b27fa68040fba4340ac
PixelatedPope <Pixelated_Pope@hotmail.com>
2025-08-26 10:06:13 -0600

    Added copy of done command "despacito" to mess with Cecil

---
+++ src/commands/guild/despacito.ts
@@ -0,0 +1,30 @@
+import { config } from '@/data/config.js';
+import { moveChannelToUnbusy } from '@/misc/help-channel-ticker.js';
+import { EmbedBuilder, SlashCommandBuilder, TextChannel } from 'discord.js';
+
+const command = new SlashCommandBuilder()
+  .setName('despacito')
+  .setDescription(`Also marks this channel as not busy (help channels only!)`)
+
+export const cmd: BotCommand = {
+  command,
+  execute: async interaction => {
+    if (!interaction.inGuild() || !interaction.guild || !interaction.channel) return;
+    if (config.discordIds.channels.helpChannels.includes(interaction.channelId)) {
+      const embed = new EmbedBuilder()
+        .setColor(config.defaultEmbedColor)
+        .setDescription(`This channel is now available for another question ${config.discordIds.emojis.duckycode}`)
+        .setTimestamp(new Date())
+
+      await interaction.reply({ embeds: [embed] });
+
+      const channel = await interaction.channel.fetch();
+      await moveChannelToUnbusy(channel as TextChannel);
+    } else {
+      await interaction.reply({
+        content: 'This command can only be used in a help channel!',
+        ephemeral: true
+      });
+    }
+  }
+};
